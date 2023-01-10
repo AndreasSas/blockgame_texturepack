0a3ee55ce4124f0043ad0b53a842fc5c3f14c900
DirectiveAthena <directive.athena@gmail.com>
2023-01-10 20:51:25 +0100

    Feat: Working flat model

--- scripts/builder/main.py
+++ scripts/builder/main.py
@@ -18,19 +18,19 @@ import zipfile
 # - Support Stuff -
 # ----------------------------------------------------------------------------------------------------------------------
 EXCEL_HEADERS:list[str] = [
-    "parent_item", "custom_model_data", "model", "model_location", "internal_name", "link_to_bbmodel", "link_to_texture"
+    "type","original_item", "custom_model_data", "location", "internal_name", "link_to_bbmodel", "link_to_texture", "command"
 ]
 SETTINGS_PATH = pathlib.Path("scripts/builder/settings.json")
 
 @dataclasses.dataclass(slots=True)
 class CustomModel:
-    parent_item:str
+    type_:str
     custom_model_data:int
-    model:str
-    model_location:str
+    location:str
+    original_item:str
 
     def output_overrides(self) -> dict:
-        return {"predicate": {"custom_model_data":self.custom_model_data}, "model": self.model}
+        return {"predicate": {"custom_model_data":self.custom_model_data}, "model": self.location}
 
 # ----------------------------------------------------------------------------------------------------------------------
 # - Functionality -
@@ -51,10 +51,10 @@ def process_excel(excel_file:pathlib.Path) -> dict[str,list[CustomModel]]:
     #   and process the data
     excel_data:pd.DataFrame = pd.read_excel(excel_file)
 
-    if set(excel_data.columns) != set(EXCEL_HEADERS):
-        raise TypeError("Excel file does not satisfy the correct order of columns:")
+    if set(cols:=excel_data.columns) != set(EXCEL_HEADERS):
+        raise TypeError(f"Excel file does not satisfy the correct order of columns:\n{EXCEL_HEADERS}\nvs\n{cols}")
 
-    for parent_item, cmd, model, model_location, *_ in pd.read_excel(excel_file).values:
+    for type_, original_item, cmd, location, *_ in pd.read_excel(excel_file).values:
         # Check the custom_model_data and cast to an int,
         #   because the json output has to use an int as well
         custom_model_data = int(cmd)
@@ -63,55 +63,51 @@ def process_excel(excel_file:pathlib.Path) -> dict[str,list[CustomModel]]:
 
         # Makes sure the parent item is present in the data dict
         #   Without this, we'd have to have a couple more if checks, while now dict handles it
-        data.setdefault(parent_item, [])
+        data.setdefault(original_item, [])
 
         # Create model and store
-        data[parent_item].append(CustomModel(
-            parent_item=parent_item,
+        data[original_item].append(CustomModel(
+            type_=type_,
             custom_model_data=custom_model_data,
-            model=model,
-            model_location=model_location
+            location=location,
+            original_item=original_item
         ))
 
     return data
 
 def create_json_files(original_folder:pathlib.Path, data:dict[str,list[CustomModel]]):
-    for parent, custom_models in data.items():
-        with open(original_folder.joinpath(f"assets/minecraft/models/{parent}.json"), "w+") as file:
+    for original_item, custom_models in data.items():
+        with open(original_folder.joinpath(f"assets/minecraft/models/{original_item.split(':')[-1]}.json"), "w+") as file:
             file.write(json.dumps(
-                {"parent": parent, "overrides": [model.output_overrides() for model in custom_models]},
+                {
+                    "parent": f"item/generated",
+                    "textures": {"layer0":original_item},
+                    "overrides": [model.output_overrides() for model in custom_models]
+                },
                 indent=2
             ))
 
-def pack_assembler(original_folder:pathlib.Path, output_file:pathlib.Path, *, excluded_filetypes:list[str]|tuple[str,...]=None, overwrite:bool=False):
+def pack_assembler(original_folder:pathlib.Path, output_files:list[pathlib.Path], *, excluded_filetypes:list[str]|tuple[str,...]=None):
     if excluded_filetypes is None:
         excluded_filetypes = []
 
     # check if the zipfile is already present
     #   Overwrites if allowed
-    if output_file.is_file():
-        if not overwrite:
-            raise FileExistsError(
-                "A zipfile was found at the 'output_file' location." 
-                " Overwriting an existing zipfile is not enabled"
-            )
-        else:
-            os.remove(output_file)
-
-    # The zip file always has to be closed, so use with
-    with zipfile.ZipFile(output_file, "w", zipfile.ZIP_DEFLATED) as zip_file:
-        for root, folders, files in os.walk(original_folder):
-            for file in filter(
-                # Uses a simple filter function to exclude those files which are excluded
-                lambda f: not f.endswith(tuple(excluded_filetypes)),
-                files
-            ):
-                # Writes to zip
-                #   arcname is used to define a new path to put the file under. We stripped the base folder out of this
-                zip_file.write(
-                    filename = (full_path := os.path.join(root, file)),
-                    arcname = pathlib.Path(*pathlib.Path(full_path).parts[1:])
-                )
+    for output_file in output_files:
+        # The zip file always has to be closed, so use with
+        with zipfile.ZipFile(output_file, "w", zipfile.ZIP_DEFLATED) as zip_file:
+            for root, folders, files in os.walk(original_folder):
+                for file in filter(
+                    # Uses a simple filter function to exclude those files which are excluded
+                    lambda f: not f.endswith(tuple(excluded_filetypes)),
+                    files
+                ):
+                    # Writes to zip
+                    #   arcname is used to define a new path to put the file under. We stripped the base folder out of this
+                    zip_file.write(
+                        filename = (full_path := os.path.join(root, file)),
+                        arcname = pathlib.Path(*pathlib.Path(full_path).parts[1:])
+                    )
 
 # ----------------------------------------------------------------------------------------------------------------------
 # - Code -
@@ -143,9 +139,11 @@ def main():
     version_str = '.'.join(str(v) for v in settings["version"])
     pack_assembler(
         original_folder=pathlib.Path(settings["original_folder"]),
-        output_file=pathlib.Path(settings["output_file".format(version_str=version_str)]),
-        excluded_filetypes=settings["excluded_files"],
-        overwrite=True
+        output_files=[
+            pathlib.Path(file.format(version_str=version_str))
+            for file in settings["output_files"]
+        ],
+        excluded_filetypes=settings["excluded_files"]
     )
 
 if __name__ == '__main__':
